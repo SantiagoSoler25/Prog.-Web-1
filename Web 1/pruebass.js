e4679724cf76ed9a9b1b9e3dcdf42674bd6a9b4d
Santiago Soler Echeverri <102199110+SantiagoSoler25@users.noreply.github.com>
2025-03-29 19:05:14 -0500

    materialize y medicos form

---
+++ Web 1/pruebass.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+  $(document).ready(function() {
+    M.updateTextFields();
+  });
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
